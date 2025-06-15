58cc75f137c4b095ab2cf05e887476861f4659e7
zeitunmohamed <zeitunmohamed@gmail.com>
2025-06-15 21:40:32 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
+   
+    
     # Class body goes here
 
     #Instance method definition
-    pass
+    
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
     # Class body goes here
 
     #Instance method definition
-    pass
+    
